34eb65478db4fb7d6f5b821b8e3ac9b88364ae6e
Moritz Reuter <moritz.reuter2@web.de>
2019-07-12 08:25:12 +0200

    aosidf

--- app/src/main/java/com/example/panplaner/ProjectsFragment.kt
+++ app/src/main/java/com/example/panplaner/ProjectsFragment.kt
@@ -14,6 +14,7 @@ import android.os.Parcelable
 import com.xwray.groupie.ViewHolder
 import android.util.Log
 import android.widget.Toast
+import androidx.core.view.forEach
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -23,18 +24,23 @@ import com.xwray.groupie.Item
 import com.xwray.groupie.OnItemClickListener
 import kotlinx.android.synthetic.main.fragment_projects.*
 import kotlinx.android.synthetic.main.project_list_item.view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class ProjectsFragment : Fragment() {
     private var listener: OnFragmentInteractionListener? = null
     val frag = "ProjectsFragment"
-
     private lateinit var projectsRef: DatabaseReference
 
     private var projectListener: ValueEventListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        GlobalScope.launch(Dispatchers.Main){
+            loadProjects()
+        }
     }
 
     override fun onCreateView(
@@ -42,20 +48,20 @@ class ProjectsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        Log.d(frag, "onCreateView")
         return inflater.inflate(R.layout.fragment_projects, container, false)
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         val uid = FirebaseAuth.getInstance().uid
         projectsRef = FirebaseDatabase.getInstance().reference.child("/Projects")
-
-        val data = loadProjects()
         add_project_button.setOnClickListener {
             val action = ProjectsFragmentDirections.actionProjectsFragmentToCreateProjectFragment()
             findNavController().navigate(action)
@@ -76,57 +82,45 @@ class ProjectsFragment : Fragment() {
         val PROJECT_KEY = "PROJECT_KEY"
     }
 
-    private fun loadProjects(){
+    suspend private fun loadProjects() {
+        val ref = FirebaseDatabase.getInstance().getReference("/users").child("${FirebaseAuth.getInstance().uid}").child("projects")
         rv_projects.apply {
             layoutManager = LinearLayoutManager(this@ProjectsFragment.context)
             val adapter = GroupAdapter<ViewHolder>()
             adapter.apply {
-                val ref = FirebaseDatabase.getInstance().getReference("/Projects").child("${FirebaseAuth.getInstance().uid}")
-                ref.addListenerForSingleValueEvent(object: ValueEventListener {
+                ref.addListenerForSingleValueEvent(object : ValueEventListener {
                     override fun onDataChange(p0: DataSnapshot) {
                         Log.d(frag, "$p0")
-                        p0.children.forEach{
+                        p0.children.forEach {
                             Log.d(frag, "getting data..")
-                            val project = it.getValue(Project::class.java)
-                            Log.d(frag, project.toString())
-                            adapter.add(ProjectItem(project))
+                            Log.d(frag, it.value.toString())
+                            val refP = FirebaseDatabase.getInstance().getReference("Projects").child("${it.value}")
+                            refP.addListenerForSingleValueEvent(object : ValueEventListener {
+                                override fun onDataChange(p0: DataSnapshot) {
+                                    p0.children.forEach {
+                                        Log.d(frag, "looking for project")
+                                        val project = it.getValue(Project::class.java)
+                                        adapter.add(ProjectItem(project))
+                                    }
+                                    adapter.setOnItemClickListener { item, view ->
+                                        val projectItem = item as ProjectItem
+                                        val intent = Intent(activity, UserActivity::class.java)
+                                        intent.putExtra(PROJECT_KEY, projectItem.project)
+                                        startActivity(intent)
+                                    }
+                                }
+                                override fun onCancelled(p0: DatabaseError) {
+                                }
+                            })
                         }
-                        adapter.setOnItemClickListener { item, view ->
-                            val projectItem = item as ProjectItem
-                            val intent = Intent(activity, UserActivity::class.java)
-                            intent.putExtra(PROJECT_KEY, projectItem.project)
-                            startActivity(intent)
-                        }
-                        rv_projects.adapter = adapter
                     }
                     override fun onCancelled(p0: DatabaseError) {
                         Toast.makeText(activity, "somethin went wrong...", Toast.LENGTH_SHORT).show()
                     }
                 })
-            }
-        }
-    }
-    private val onItemClick = OnItemClickListener { item, view ->
-        if(item is ProjectItem){
+                rv_projects.adapter = adapter
 
-            Log.d(frag, "clicked ${item.project?.name.toString()}")
-            Log.d(frag, "clicked ${item.project?.uid.toString()}")
-            Log.d(frag, "clicked ${item.project?.creator.toString()}")
-            Log.d(frag, "clicked ${item.project?.deadline.toString()}")
-            val intent = Intent(context, UserActivity::class.java)
-
-            /*var data:MutableList<ProjectItem> = ArrayList<ProjectItem>()
-            data.add(item)
-            for (item in data) {
-                Log.d(frag, "$item")
             }
-            intent.putParcelableArrayListExtra("project", data as java.util.ArrayList<out Parcelable>)
-            */
-            intent.putExtra("projectName",item.project?.name)
-            intent.putExtra("projectUid",item.project?.uid)
-            intent.putExtra("projectDeadline",item.project?.deadline)
-            intent.putExtra("projectUsers", item.project?.users)
-            startActivity(intent)
         }
     }
 
